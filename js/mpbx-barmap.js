1df4401dcecd2c0b13aaa32b389125b4082e1374
juanrugar <jruigar138@gmail.com>
2023-09-08 13:59:46 +0200

    Basic controls added

--- js/mpbx-barmap.js
+++ js/mpbx-barmap.js
@@ -1,23 +1,25 @@
 
+let 
+
 // Start init()
-
 function init() {
-//access token
-mapboxgl.accessToken = 'pk.eyJ1IjoianVhbnJ1Z2FyIiwiYSI6ImNranlpd2lqbzB5djQydW1sZTRvem9tdmgifQ.zvNvGIYji6wVpwjlnTHeJg';
 
-const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v12', // style URL
-center: [-0.37918, 39.4741], // starting position [lng, lat] = Valencia
-zoom: 9, // starting zoom
+    //Access TOKEN
+    mapboxgl.accessToken = 'pk.eyJ1IjoianVhbnJ1Z2FyIiwiYSI6ImNranlpd2lqbzB5djQydW1sZTRvem9tdmgifQ.zvNvGIYji6wVpwjlnTHeJg';
 
-});
+    //MAP DECLARATION
+    const map = new mapboxgl.Map({
+    container: 'map-canvas', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-0.37918, 39.4741], // starting position [lng, lat] = Valencia
+    zoom: 14, // starting zoom
 
-}   
+    });
 
+    //CONTROLS
+    map.addControl(new mapboxgl.AttributionControl({ compact: true }));
+    map.addControl(new mapboxgl.NavigationControl());
+
+};
 // End init()
 
-// controls
-
-
- //
